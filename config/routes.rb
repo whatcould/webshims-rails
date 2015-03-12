b3f81e3d423f0a32447064f376e6f904887a3974
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2015-03-12 09:42:34 -0700

    Use Rack::File instead of custom code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   if ::Rails::VERSION::MAJOR < 4
-    mount Webshims::Rails::Rewrite, :at => "/assets/webshims/shims"
+    mount Webshims::Rails::Rewrite.new, :at => "/assets/webshims/shims"
   else
-    mount Webshims::Rails::Rewrite, :at => "/webshims/shims"
+    mount Webshims::Rails::Rewrite.new, :at => "/webshims/shims"
   end
 end
--- lib/webshims-rails/rewrite.rb
+++ lib/webshims-rails/rewrite.rb
@@ -1,36 +1,16 @@
 module Webshims
   module Rails
-    class Rewrite
-      class << self
-        def call(env)
-          filename = path(env)
-          body = File.read(filename)
-          [
-              200,
-              {
-                  "Content-Type" => content_type(filename),
-                  "Content-Length" => bytesize(body)
-              },
-              [body]
-          ]
-        end
+    class Rewrite < Rack::File
 
-        private
+      def initialize
+        env[PATH_INFO] = env[PATH_INFO].gsub(%r(\A/[0-9.]+/), '')
+        super(root)
+      end
 
-        def path(env)
-          filename = env["PATH_INFO"].gsub(%r(\A/[0-9.]+/), '')
-          File.join(Gem.loaded_specs['webshims-rails'].full_gem_path, "vendor", "assets", "javascripts", "webshims", "shims", filename)
-        end
+      private
 
-        def content_type(filename)
-          extname = File.extname(filename)[1..-1]
-          mime_type = Mime::Type.lookup_by_extension(extname)
-          mime_type.to_s
-        end
-
-        def bytesize(content)
-          "".respond_to?(:bytesize) ? content.bytesize.to_s : content.size.to_s
-        end
+      def root
+        File.join(Gem.loaded_specs['webshims-rails'].full_gem_path, "vendor", "assets", "javascripts", "webshims", "shims")
       end
     end
   end
