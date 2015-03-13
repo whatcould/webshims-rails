af357a048ea9adc38bab154a823cceb16c9ac76f
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2015-03-13 16:13:25 -0700

    Er.. there is no env in initialize

--- lib/webshims-rails/rewrite.rb
+++ lib/webshims-rails/rewrite.rb
@@ -3,14 +3,18 @@ module Webshims
     class Rewrite < Rack::File
 
       def initialize
-        env[PATH_INFO] = env[PATH_INFO].gsub(%r(\A/[0-9.]+/), '')
         super(root)
+      end
+
+      def call(env)
+        env["PATH_INFO"] = env["PATH_INFO"].gsub(%r(\A/[0-9.]+/), '')
+        super(env)
       end
 
       private
 
       def root
-        File.join(Gem.loaded_specs['webshims-rails'].full_gem_path, "vendor", "assets", "javascripts", "webshims", "shims")
+        File.join(Gem.loaded_specs["webshims-rails"].full_gem_path, "vendor", "assets", "javascripts", "webshims", "shims")
       end
     end
   end
