18d062fbb55ad47a59a8b7fc2b56f67297910403
Jeremy Wadsack <jeremy.wadsack@gmail.com>
2015-03-14 16:47:29 -0700

    Also remove "assets" from view helper

--- lib/webshims-rails/view-helpers.rb
+++ lib/webshims-rails/view-helpers.rb
@@ -2,9 +2,7 @@ module Webshims
   module Rails
     module ViewHelpers
       def webshims_path
-        path = "/webshims/shims/#{Webshims::Rails::WEBSHIMS_VERSION}/"
-        path = "/assets/#{path}" if ::Rails::VERSION::MAJOR < 4
-        path
+        "/webshims/shims/#{Webshims::Rails::WEBSHIMS_VERSION}/"
       end
     end
   end
